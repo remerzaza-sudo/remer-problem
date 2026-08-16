97627e7a27fe75648f61eb2315c0975dd6cc08f9
remerzaza-sudo <remerzaza@gmail.com>
2026-08-16 22:07:48 +0700

    Update studx.py

--- studx.py
+++ studx.py
@@ -1,6 +1,7 @@
 gpa = []
 count = 0
-while count < 4:
+while usersdatas.lower() != 'q':
+
     usersdatas = input(f"class({count+1}):")
     for value in usersdatas.split():
         if value == "end":
@@ -11,6 +12,3 @@ while count < 4:
         else:
             print("invavid data")
     count += 1
-
-for i in gpa:
-    print(i)
