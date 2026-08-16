83f66b2afa21c790c9f8151052a81775fa5b8812
remerzaza-sudo <remerzaza@gmail.com>
2026-08-16 22:37:36 +0700

    Update code First section

--- studx.py
+++ studx.py
@@ -1,13 +1,20 @@
 gpa = []
 count = 0
-while usersdatas.lower() != 'q':
+usersdatas = ""
+run = False
+while not run:
     usersdatas = input(f"class({count+1}):")
     for value in usersdatas.split():
-        if value == "end":
+        if value == "q":
+            run = True
+            break
+        
+        elif value == "n":
             break
         usersdata = float(value)
         if 0 < usersdata <= 4.00:
             gpa.append(usersdata)
         else:
             print("invavid data")
+
     count += 1
