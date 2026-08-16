1f7cd75a5bb1c89eae9e7d12a0d9e472e320a170
monody <respexeditor@gmail.com>
2026-08-17 00:03:29 +0700

    Update studx.py

--- studx.py
+++ studx.py
@@ -1,19 +1,26 @@
-gpa = []
+rawgpa = []
 count = 0
 usersdatas = ""
 run = False
+print("Enter your GPA data (type 'n' to move to the next class or 'q' to quit):")
 while not run:
     usersdatas = input(f"class({count+1}):")
     for value in usersdatas.split():
         if value == "q":
             run = True
             break
-        
         elif value == "n":
             break
         usersdata = float(value)
         if 0 < usersdata <= 4.00:
-            gpa.append(usersdata)
+            num = [value]
+            rawgpa.append(num)
         else:
             print("invalid data")
     count += 1
+
+
+
+
+    
+
